98193a02b52c05306d89ce9beb9d2da1f2bec9bc
Irvin Cueto <iirvin.cueto.02@gmail.com>
2023-04-27 00:01:36 -0600

    validacion

--- app/Http/Controllers/AuthUser/mainUserController.php
+++ app/Http/Controllers/AuthUser/mainUserController.php
@@ -3,26 +3,40 @@
 namespace App\Http\Controllers\AuthUser;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Aula;
+use App\Models\actividad;
+use App\Models\usuario;
 
 
 class mainUserController extends Controller
 {
     //
-    public function mainUser(){
-        $aulas = Aula::all();
 
-        return view('user.mainUser', compact('aulas'));
-    }
+    public function mainUser(Request $request, $idUsuario, $correoUsuario)
+    {
+        // desencriptar el correo electrónico
+        $correoUsuario = decrypt($correoUsuario);
 
+        // obtener el usuario autenticado
+        $user = Usuario::find($idUsuario);
 
-    public function searchAula(Request $request){
-        $query = $request->get('query');
-        $aulas = Aula::where('nombreAula', 'like', '%' . $query . '%')
-                      ->orWhere('idAula', $query)
-                      ->get();
-        return view('user.mainUser', compact('aulas'));
+        // asegurarse de que el usuario autenticado es el mismo que se está buscando
+        if ($user && $user->correoUsuario === $correoUsuario) {
+            // obtener el valor de la consulta
+            $query = $request->input('query');
+
+            // obtener las aulas que coinciden con la consulta
+            $aulas = Aula::where('nombreAula', 'like', '%' . $query . '%')->paginate(5);
+
+            // obtener el nombre del usuario autenticado
+            $nombreUsuario = Auth::guard('usuario')->user()->nombreUsuario;
+
+            // pasar las variables a la vista
+            return view('user.mainUser', compact('nombreUsuario', 'aulas', 'idUsuario', 'correoUsuario'));
+        } else {
+            abort(404);
+        }
     }
 }
-
--- resources/views/archivoBaseUser/baseUser.blade.php
+++ resources/views/archivoBaseUser/baseUser.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,45 +8,52 @@
 
     <title>@yield('title')</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/2400098b91.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('css/baseUser.css') }}">
 
 </head>
+
 <body>
 
     <div class="container">
         <header class="d-flex flex-wrap justify-content-center py-3">
             <a href="/" class="d-flex align-items-center mb-md-0 me-md-auto text-dark text-decoration-none mx-5">
-              <span class="fs-1 mx-2"> <strong> SACAT</strong></span>
+                <span class="fs-1 mx-2"> <strong> SACAT</strong></span>
             </a>
             <form action="{{ route('logout-user') }}" method="POST" style="display: inline;">
-              @csrf
+                @csrf
 
             </form>
             <form action="{{ route('logout-user') }}" method="POST" style="display: inline;">
                 @csrf
                 <button type="submit" class="nav-link btn-unstyled ">
-                  <ul class="nav nav-pills">
-                    <li class="nav-item mx-3"><a href="#" class="nav-link"> <i class="fa-regular fa-circle-user icon-header fa-3x"></i> </a></li>
-                  </ul>
+                    <ul class="nav nav-pills">
+                        <li class="nav-item mx-3"><a href="#" class="nav-link"> <i
+                                    class="fa-regular fa-circle-user icon-header fa-3x"></i> </a></li>
+                    </ul>
                 </button>
             </form>
-          </header>
-          <div class="content-user ">
+        </header>
+        <div class="content-user ">
             <div class="nombreUser">
-                <h2 class="text-start text-white">Bienvenido {{ session('nombreUsuario') }}</h2>
+                <h2 class="text-start text-white">Bienvenido @yield('nombreUsuario')</h2>
+
             </div>
             <div class="fechaUser">
                 <h2 class="text-end">Fecha: {{ date('d/m/Y') }}</h2>
             </div>
-          </div>
+        </div>
     </div>
 
 
     @yield('content')
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
 
 </body>
+
 </html>
