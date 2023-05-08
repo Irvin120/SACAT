d06c70511ecc11c2f430844569fa865b72488328
kikitosomar <kikesomaregente@gmail.com>
2023-05-08 00:49:59 -0600

    mauqetado realizar actividad

--- resources/views/user/checklisUser.blade.php
+++ resources/views/user/checklisUser.blade.php
@@ -8,35 +8,42 @@
 
 @section('content')
 
-    {{-- <link rel="stylesheet" href=" {{ asset('css/user/checklisUser.css') }}"> --}}
+    <link rel="stylesheet" href=" {{ asset('css/user/checklisUser.css') }}">
 
     <div class="content">
 
-        <div class="row mb-4">
-            <div class="col-md-12">
-                <h2 class="font-weight-bold">{{ $actividad->nombreActividad }}</h2>
-                <p class="mb-0">{{ Str::ucfirst($actividad->resumen) }}</p>
+        <!-- información de la actividad -->
+            <div class="conten-info">
+                <!-- nombre de la actividad -->
+                <h2 class="titulo font-weight-bold">{{ $actividad->nombreActividad }}</h2>
+                <!-- indicaciones de la actividad -->
+                <p class="resumen mb-0">{{ Str::ucfirst($actividad->resumen) }}</p>
             </div>
-        </div>
 
-        <div class="row mb-4">
-            <div class="col-md-12">
-                <h5 class="font-weight-bold">Fechas</h5>
-                <p class="mb-0"><span class="font-weight-bold">Inicio:</span> {{ $actividad->fechaInicio }} <br> <span
-                        class="font-weight-bold">Fin:</span> {{ $actividad->fechaFin }}</p>
+        <!-- fecha de la actividad -->
+            <div class="fecha-activ">
+                <!-- titilo fecha -->
+                <p class="text-f font-weight-bold mb-0">Fechas:</p>
+                <!-- inicio y fin de la actividad -->
+                <p class="mb-0 text-p">
+                    <span class="font-weight-bold text-inicio">Inicio:</span> 
+                    {{ $actividad->fechaInicio }}
+                    <span class="font-weight-bold text-fin">Fin:</span> 
+                    {{ $actividad->fechaFin }}
+                </p>
             </div>
-        </div>
 
-        <div class="row mb-4">
-            <div class="col-md-12">
-                <h5 class="font-weight-bold">Días</h5>
+        <div class="conten-conten">
+
+            <div class="conten-actions">
+                <h5 class="text-dias font-weight-bold ">Días por entregar</h5>
                 {{-- {{ dd($dias) }} --}}
 
 
                 <div id="error-message" style="display:none">{{ session('error') }}</div>
 
 
-                <form method="post" action="{{ route('registrarActividad') }}">
+                <form class="conten-check" method="post" action="{{ route('registrarActividad') }}">
                     @csrf
 
 
@@ -46,7 +53,7 @@
 
 
                     @foreach ($dias as $dia)
-                        <form method="post" action="{{ route('registrarActividad') }}">
+                        <form class="conten-check" method="post" action="{{ route('registrarActividad') }}">
                             @csrf
                             <input type="hidden" name="idActividad" value="{{ $actividad->idActividad }}">
                             <input type="hidden" name="idUsuario" value="{{ $usuario->idUsuario }}">
@@ -55,35 +62,41 @@
 
                             <div class="form-check">
                                 @if (!$registrosUsuario->contains('fechaRegistroActividad', $dia))
+                                    <!-- cuadro de checkbox     -->
                                     <input class="form-check-input resumen-checkbox" type="checkbox" required name="dias[]"
-                                        value="{{ $dia }}" id="{{ $dia }}">
+                                    value="{{ $dia }}" id="{{ $dia }}">
                                 @endif
 
-                                <label class="form-check-label" for="{{ $dia }}">
+                                <!-- fecha con nombre de dia -->
+                                <label class="fecha form-check-label" for="{{ $dia }}">
                                     {{ date('d/m/Y', strtotime($dia)) }} -
                                     {{ \Carbon\Carbon::parse($dia)->locale('es')->dayName }}
                                 </label>
-                                @if ($registrosUsuario->contains('fechaRegistroActividad', $dia))
-                                    <input type="hidden" name="resumenes[{{ $dia }}]"
-                                        value="{{ $registrosUsuario->where('fechaRegistroActividad', $dia)->first()->resumenRegistroActividad }}">
-                                    <input type="text"
-                                        value="{{ $registrosUsuario->where('fechaRegistroActividad', $dia)->first()->resumenRegistroActividad }}"readonly>
-                                @else
-                                    <input type="text" name="resumenes[{{ $dia }}]"
-                                        placeholder="Resumen del día" class="resumen-input">
-                                @endif
                             </div>
 
+                            <!-- input para anotaciones -->
+                            <div class="conten-inputs">                            
+                                @if ($registrosUsuario->contains('fechaRegistroActividad', $dia))
+                                    <input type="hidden" name="resumenes[{{ $dia }}]" class="resumen-input"
+                                        value="{{ $registrosUsuario->where('fechaRegistroActividad', $dia)->first()->resumenRegistroActividad }}">
+                                    <input type="text" class="resumen-input"
+                                        value="{{ $registrosUsuario->where('fechaRegistroActividad', $dia)->first()->resumenRegistroActividad }}"readonly>
+                                        
+                                    <!-- <textarea type="text" value="{{ $registrosUsuario->where('fechaRegistroActividad', $dia)->first()->resumenRegistroActividad }}"readonly
+                                    class="form-control" id="exampleFormControlTextarea1" rows="3">
+                                    </textarea> -->
+                                @else
+                                    <textarea type="text" name="resumenes[{{ $dia }}]" placeholder="Resumen del día" class="resumen-input" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                @endif
 
-                            <button type="submit" class="btn btn-primary mt-4">Enviar</button>
+
+                                <button type="submit" class="btn-enviar btn btn-primary">Enviar</button>
+                            </div>
                         </form>
                     @endforeach
 
 
                 </form>
-
-
-            </div>
         </div>
 
     </div>
