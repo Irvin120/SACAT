c7018db5b79ce0e7a1ceeba372bafa8add058409
kikitosomar <kikesomaregente@gmail.com>
2023-05-12 21:52:45 -0600

    revicion de actividades

--- resources/views/admin/revisionActividad.blade.php
+++ resources/views/admin/revisionActividad.blade.php
@@ -1,5 +1,7 @@
 @extends('archivoBaseAdmin.baseAdmin')
 
+<link rel="stylesheet" href="{{ asset('css/admin/revisionActividades.css') }}">
+
 @section('botones')
     <li class="nav-item mx-3 d-flex justify-content-center align-items-center">
         <a href="#" onclick="window.history.back();" class="text-white text-decoration-none">Volver</a>
@@ -8,48 +10,60 @@
 
 
 @section('content')
-    <div class="container">
-        <div class="mb-3 text-center">
-            <h2>{{ $actividad->nombreActividad }}</h2>
+
+    <!-- contenedor principal -->
+    <div class="contain-pr">
+
+        <!-- titulo -->
+        <div class="cont-titulo mb-3 text-center">
+            <!-- text-titulo -->
+            <h2 class="text-titulo">{{ $actividad->nombreActividad }}</h2>
         </div>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Día</th>
-                    @foreach ($dias as $dia)
-                        <th> {{ date('d/m/Y', strtotime($dia)) }} -
-                            {{ \Carbon\Carbon::parse($dia)->locale('es')->dayName }}</th>
-                    @endforeach
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($usuarios as $usuario)
-                    <tr>
-                        <td>{{ $usuario->nombreUsuario }}</td>
+
+        <!-- contenedor tabla -->
+        <div class="conten-tabla">
+            <!-- tabla -->
+            <table class="tabla">
+                <!-- encabezado de la tabla -->
+                <thead class="encabezado">
+                    <tr class="coconten-encabezado">
+                        <!-- titulo dia -->
+                        <th class="titulo-dia">Día</th>
                         @foreach ($dias as $dia)
-                            <td>
-                                @php
-                                    $registro = $registros
-                                        ->where('idUsuario', $usuario->idUsuario)
-                                        ->where('fechaRegistroActividad', $dia)
-                                        ->first();
-                                @endphp
-                                {{ $registro ? $registro->resumenRegistroActividad : '-' }}
-                            </td>
+                            <!-- fecha y dia -->
+                            <th class="nombre-dia"> {{ date('d/m/Y', strtotime($dia)) }} -
+                                {{ \Carbon\Carbon::parse($dia)->locale('es')->dayName }}
+                            </th>
                         @endforeach
                     </tr>
-                @endforeach
-            </tbody>
+                </thead>
 
+                <!-- contenido de la tabla -->
+                <tbody class="conten-contenido-tabla">
+                    @foreach ($usuarios as $usuario)
+                        <tr class="contenido-tabla">
 
+                            <!-- nombre del usuario -->
+                            <td class="nombre-usuario">{{ $usuario->nombreUsuario }}</td>
+                            @foreach ($dias as $dia)
+                                <!-- resumen -->
+                                <td class="resumen">
+                                    @php
+                                        $registro = $registros
+                                            ->where('idUsuario', $usuario->idUsuario)
+                                            ->where('fechaRegistroActividad', $dia)
+                                            ->first();
+                                    @endphp
+                                    {{ $registro ? $registro->resumenRegistroActividad : '-' }}
+                                </td>
+                            @endforeach
 
+                        </tr>
+                    @endforeach
+                </tbody>
 
-
-
-
-        </table>
-
-    </div>
+            </table>
+        </div>
 
     </div>
 @endsection
